8168469185bac8778539c309b0bb68eee15a206f
tom Koren <tom.koren@tensorleap.ai>
2022-01-18 16:22:55 +0200

    added tensorleap and poetry files

---
+++ examples/coco/.tensorleap/model.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from coco.model import unet_model
+import tensorflow as tf
+
+OUTPUT_CLASSES = 4
+
+
+def leap_save_model(target_file_path: Path):
+    # Load your model
+    # Save it to the path supplied as an arugment (has a .h5 suffix)
+    model = unet_model(output_channels=OUTPUT_CLASSES)
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    model.save(target_file_path)
