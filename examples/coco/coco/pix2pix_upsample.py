7416df851bf7bc0879665359d3c10da2e45ed28b
tom Koren <tom.koren@tensorleap.ai>
2022-01-18 16:15:08 +0200

    replaced convtranspose2D with upsample + conv2d and added code license and origin

--- examples/coco/coco/pix2pix_upsample.py
+++ examples/coco/coco/pix2pix_upsample.py
@@ -1,3 +1,23 @@
+# Taken from https://github.com/tensorflow/examples/blob/master/tensorflow_examples/models/pix2pix/pix2pix.py
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# This was adapted to the TF platform by changing the sequential model to a regular
+"""Pix2pix.
+"""
+
 import tensorflow as tf
 
 
@@ -43,12 +63,9 @@ def upsample(filters, size, norm_type='batchnorm', apply_dropout=False):
   initializer = tf.random_normal_initializer(0., 0.02)
 
   result = tf.keras.Sequential()
-  result.add(
-      tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
-                                      padding='same',
-                                      kernel_initializer=initializer,
-                                      use_bias=False))
-
+  result.add(tf.keras.layers.UpSampling2D(size=(2,2)))
+  result.add(tf.keras.layers.Conv2D(filters, size, strides=1, padding='same',
+                                    kernel_initializer=initializer, use_bias=False))
   if norm_type.lower() == 'batchnorm':
     result.add(tf.keras.layers.BatchNormalization())
   elif norm_type.lower() == 'instancenorm':
