27200abe4fd0e87623fefd3f9039014c24c01324
tom Koren <tom.koren@tensorleap.ai>
2022-01-25 10:31:07 +0200

    added a random suffix to the bucket

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "datasets" {
-  name     = "${local.client_name}-datasets"
+  name     = "${local.client_name}-datasets-${random_string.bucket_suffix.result}"
   project  = local.project_id
   location = "US"
   force_destroy = true
@@ -13,4 +13,15 @@ resource "google_storage_bucket_access_control" "public_rule" {
   bucket = local.dataset_bucket
   role   = "READER"
   entity = "allUsers"
+}
+
+resource "random_string" "bucket_suffix" {
+  length           = 8
+  upper            = false
+  special          = false
+  override_special = "/@£$"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
