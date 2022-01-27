75a2168ecd4bb098b348b28217321d721f76ef2f
Danielle Ben Bashat <daniellebenbashat@Danielles-MacBook-Pro.local>
2022-01-27 12:24:45 +0200

    more fixing dataset to run on platform

--- examples/mnist/mnist/load_data.py
+++ examples/mnist/mnist/load_data.py
@@ -5,33 +5,28 @@ from pathlib import Path
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
-
-def read_csv_file(file_path: Path):
-    df = pd.read_csv(file_path)
+def preprocess(df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
     data_X = df.drop('label', axis=1).to_numpy()
-    data_X = np.reshape(data_X, (len(data_X), 28, 28, 1))
+    data_X = np.reshape(data_X, (len(data_X), 28, 28, 1)) / 255.  # normalize to range 0-1
     data_Y = df.label.to_numpy()
+    # one hot encode the target values
+    data_Y = to_categorical(data_Y)
     return data_X, data_Y
 
 
-# load train and test mnist dataset
-def load_dataset(train_file_path: Path, test_file_path: Path) -> List[np.ndarray]:
+# load train and test mnist dataset localy
+def load_datasets(train_file_path: Path, test_file_path: Path) -> List[np.ndarray]:
 
-    # load datasets
-    train_X, train_Y = read_csv_file(train_file_path)
-    test_X, test_Y = read_csv_file(test_file_path)
+    df = pd.read_csv(train_file_path)
+    train_X, train_Y = preprocess(df)
 
-    # (train_X, train_Y), (test_X, test_Y) = mnist.load_data()   # tensorflow dataset loading API
+    df = pd.read_csv(test_file_path)
+    test_X, test_Y = preprocess(df)
 
-    # reshape dataset to have a single channel
-    train_X = train_X.reshape((train_X.shape[0], 28, 28, 1))
-    test_X = test_X.reshape((test_X.shape[0], 28, 28, 1))
     # summarize loaded dataset
     print(f"Train: X={train_X.shape[0]}, y={train_Y.shape[0]}")
     print(f"Test: X={test_X.shape[0]}, y={test_Y.shape[0]}")
-    # one hot encode the target values
-    train_Y = to_categorical(train_Y)
-    test_Y = to_categorical(test_Y)
+
     return train_X, train_Y, test_X, test_Y
 
 
