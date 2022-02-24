0d90c1448bd9f9e8a25cbea80d018790cb3affbb
tom Koren <tom.koren@tensorleap.ai>
2022-02-24 15:07:23 +0200

    switch preprocessing from json to csv

--- examples/imdb/imdb/.tensorleap/dataset.py
+++ examples/imdb/imdb/.tensorleap/dataset.py
@@ -1,23 +1,22 @@
-import os
-from typing import Union, List
-import math
-import numpy as np
-
-from code_loader.contract.enums import DatasetInputType, DatasetOutputType, DatasetMetadataType
-from code_loader.contract.datasetclasses import SubsetResponse
-from code_loader import dataset_binder
-from google.cloud import storage
-from google.cloud.storage import Bucket
-from google.auth.credentials import AnonymousCredentials
-from os.path import join
-from keras_preprocessing.text import Tokenizer as TokenizerType
-from tensorflow.keras.preprocessing.text import Tokenizer, tokenizer_from_json
 import json
-from os.path import basename, dirname
-from typing import List, Optional, Callable, Tuple, Dict, Any
+import os
 import re
 import string
+from os.path import basename, dirname
+from os.path import join
+from typing import List, Optional, Callable, Tuple, Dict
+import pandas as pd
+import numpy as np
+from code_loader import dataset_binder
+from code_loader.contract.datasetclasses import SubsetResponse
+from code_loader.contract.enums import DatasetInputType, DatasetOutputType, DatasetMetadataType
+from google.auth.credentials import AnonymousCredentials
+from google.cloud import storage
+from google.cloud.storage import Bucket
+from keras_preprocessing.text import Tokenizer as TokenizerType
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from pandas.core.frame import DataFrame as DataFrameType
 NUMBER_OF_SAMPLES = 20000
 BUCKET_NAME = 'example-datasets-47ml982d'
 PROJECT_ID = 'example-dev-project-nmrksf0o'
@@ -27,31 +26,6 @@ METRIC_NAMES = ["flesch_reading", "flesch_kincaid", "coleman_liau_index", "autom
                 "gutierrez_polini", "crawford", "gulpease_index", "osman"]
 
 
-def standartize(comment: str) -> str:
-    lowercase = comment.lower()
-    html_stripped = re.sub('<br />', ' ', lowercase)
-    punctuation_stripped = re.sub('[%s]' % re.escape(string.punctuation), '', html_stripped)
-    return punctuation_stripped
-
-
-def prepare_input(tokanizer: TokenizerType, input_text: str, sequence_length: int = 250) -> np.ndarray:
-    standard_text = standartize(input_text)
-    tokanized_input = tokanizer.texts_to_sequences([standard_text])
-    padded_input = pad_sequences(tokanized_input, maxlen=sequence_length)
-    return padded_input[0, ...]
-
-
-def get_gt(samples_path: List[str]) -> List[List[float]]:
-    # gt_list = [None]*len(samples_path)
-    gt_list = np.zeros((len(samples_path), 2), dtype=float).tolist()
-    for i in range(len(samples_path)):
-        if basename(dirname(samples_path[i])) == "pos":
-            gt_list[i] = [1.0, 0.]
-        else:
-            gt_list[i] = [0., 1.0]
-    return gt_list
-
-
 def load_tokanizer(tokanizer_path: str) -> TokenizerType:
     with open(tokanizer_path, 'r') as f:
         data = json.load(f)
@@ -59,15 +33,14 @@ def load_tokanizer(tokanizer_path: str) -> TokenizerType:
     return tokenizer
 
 
-def download_load_assets() -> Tuple[TokenizerType, Dict[str, np.ndarray]]:
-    cloud_path = join("assets", "train_dict_v5.json")
+def download_load_assets() -> Tuple[TokenizerType, DataFrameType]:
+    cloud_path = join("assets", "imdb.csv")
     local_path = _download(cloud_path)
-    with open(local_path, 'r') as f:
-        train_dict = json.load(f)
+    df = pd.read_csv(local_path)
     cloud_path = join("assets", "tokenizer_v2.json")
     local_path = _download(cloud_path)
     tokenizer = load_tokanizer(local_path)
-    return tokenizer, train_dict
+    return tokenizer, df
 
 
 def _connect_to_gcs() -> Bucket:
@@ -96,42 +69,41 @@ def _download(cloud_file_path: str, local_file_path: Optional[str] = None) -> st
 
 
 def subset_func() -> List[SubsetResponse]:
-    tokenizer, train_dict = download_load_assets()
+    tokenizer, df = download_load_assets()
     dataset_binder.cache_container["word_to_index"]["tokens"] = tokenizer.word_index
-    combined_size = 2*len(train_dict['pos'])
-    truncated_size = min(NUMBER_OF_SAMPLES, int(0.9*combined_size))
-    train_label_size = int(truncated_size/2)
-    truncated_val_size = int(0.1*truncated_size)
-    val_label_size = int(truncated_val_size/2)
-    input_keys_pre = ['', 'metrics', 'length', 'oov', 'polarity', 'subjectivity']
-    output_keys = ['paths', 'metrics', 'length', 'oov_count', 'polarity', 'subjectivity']
-    gt = ['pos', 'neg']
-    data_dict = {'train': {}, 'val': {}}
-    # Building the dictionary for the Subset response - composed of "[POS/NEG]_METRICNAME"
-    for in_key_pre, out_key in zip(input_keys_pre, output_keys):
-        if in_key_pre != '':
-            # Accessing the [POS/NEG]_METRICNAME key in the preprocessed data
-            in_key = [gt[j] + "_" + in_key_pre for j in range(len(gt))]
-        else:
-            in_key = gt
-        data_dict['train'][out_key] = train_dict[in_key[0]][:train_label_size] + \
-                                            train_dict[in_key[1]][:train_label_size]
-        data_dict['val'][out_key] = train_dict[in_key[0]][train_label_size:train_label_size+val_label_size] +\
-                                      train_dict[in_key[1]][train_label_size:train_label_size+val_label_size]
-    data_dict['train']['gt'] = get_gt(data_dict['train']['paths'])
-    data_dict['val']['gt'] = get_gt(data_dict['val']['paths'])
-    data_dict['train']['tokenizer'] = tokenizer
-    data_dict['val']['tokenizer'] = tokenizer
-    # Our train/val are composed of 50% negative and 50% positive samples
-    train = SubsetResponse(length=2*train_label_size, data=data_dict['train'])
-    val = SubsetResponse(length=2*val_label_size, data=data_dict['val'])
+    train_label_size = int(0.9 * NUMBER_OF_SAMPLES / 2)
+    val_label_size = int(0.1 * NUMBER_OF_SAMPLES / 2)
+    df = df[df['subset'] == 'train']
+    train_df = pd.concat([df[df['gt'] == 'pos'][:train_label_size],
+                         df[df['gt'] == 'neg'][:train_label_size]],
+                         ignore_index=True)
+    val_df = pd.concat([df[df['gt'] == 'pos'][train_label_size:train_label_size + val_label_size],
+                       df[df['gt'] == 'neg'][train_label_size:train_label_size + val_label_size]],
+                       ignore_index=True)
+    ohe = {"pos": [1.0, 0.], "neg": [0., 1.0]}
+    train = SubsetResponse(length=2 * train_label_size, data={"df": train_df, "tokenizer": tokenizer, "ohe": ohe})
+    val = SubsetResponse(length=2 * val_label_size, data={"df": val_df, "tokenizer": tokenizer, "ohe": ohe})
     response = [train, val]
     return response
 
 
+def standartize(comment: str) -> str:
+    lowercase = comment.lower()
+    html_stripped = re.sub('<br />', ' ', lowercase)
+    punctuation_stripped = re.sub('[%s]' % re.escape(string.punctuation), '', html_stripped)
+    return punctuation_stripped
+
+
+def prepare_input(tokanizer: TokenizerType, input_text: str, sequence_length: int = 250) -> np.ndarray:
+    standard_text = standartize(input_text)
+    tokanized_input = tokanizer.texts_to_sequences([standard_text])
+    padded_input = pad_sequences(tokanized_input, maxlen=sequence_length)
+    return padded_input[0, ...]
+
+
 # Input Encoder
 def input_tokens(idx: int, subset: SubsetResponse) -> np.ndarray:
-    comment_path = subset.data['paths'][idx]
+    comment_path = subset.data['df']['paths'][idx]
     local_path = _download(comment_path)
     with open(local_path, 'r') as f:
         comment = f.read()
@@ -142,51 +114,52 @@ def input_tokens(idx: int, subset: SubsetResponse) -> np.ndarray:
 
 # GT Encoder
 def gt_sentiment(idx: int, subset: SubsetResponse) -> List[float]:
-    return subset.data['gt'][idx]
+    gt_str = subset.data['df']['gt'][idx]
+    return subset.data['ohe'][gt_str]
 
 
 # Metadata Encoders
 def gt_metadata(idx: int, subset: SubsetResponse) -> str:
-    if subset.data['gt'][idx][0] == 1.0:
+    if subset.data['df']['gt'][idx] == "pos":
         return "positive"
     else:
         return "negative"
 
 
-def metadata_encoder(metric_idx: int) -> Callable[[int, SubsetResponse], float]:
+def metadata_encoder(metric_idx: str) -> Callable[[int, SubsetResponse], float]:
     def func(idx: int, subset: SubsetResponse) -> float:
-        return subset.data['metrics'][idx][metric_idx]
+        return subset.data['df'][METRIC_NAMES[metric_idx]][idx]
+
     func.__name__ = METRIC_NAMES[metric_idx]
     return func
 
 
 def length_metadata(idx: int, subset: SubsetResponse) -> int:
-    return subset.data['length'][idx]
+    return subset.data['df']['length'][idx]
 
 
 def oov_metadata(idx: int, subset: SubsetResponse) -> int:
-    return subset.data['oov_count'][idx]
+    return subset.data['df']['oov_count'][idx]
 
 
 def score_metadata(idx, subset: SubsetResponse) -> int:
-    return int(subset.data['paths'][idx].split("_")[1].split(".")[0])
+    return int(subset.data['df']['paths'][idx].split("_")[1].split(".")[0])
 
 
 def score_confidence_metadata(idx, subset: SubsetResponse) -> int:
-    return abs(5 - int(subset.data['paths'][idx].split("_")[1].split(".")[0]))
+    return abs(5 - int(subset.data['df']['paths'][idx].split("_")[1].split(".")[0]))
 
 
 def polarity_metadata(idx, subset: SubsetResponse) -> float:
-    return subset.data['polarity'][idx]
+    return subset.data['df']['polarity'][idx]
 
 
 def subjectivity_metadata(idx, subset: SubsetResponse) -> float:
-    return subset.data['subjectivity'][idx]
+    return subset.data['df']['subjectivity'][idx]
 
 
 # Binders
 dataset_binder.set_subset(function=subset_func, name='IMDBComments')
-
 
 dataset_binder.set_input(function=input_tokens, subset='IMDBComments', input_type=DatasetInputType.Text,
                          name='tokens')
@@ -196,35 +169,34 @@ dataset_binder.set_ground_truth(function=gt_sentiment, subset='IMDBComments',
                                 name='sentiment', labels=['positive', 'negative'], masked_input=None)
 
 dataset_binder.set_metadata(function=gt_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.string,
-                                name='gt')
+                            metadata_type=DatasetMetadataType.string,
+                            name='gt')
 
 dataset_binder.set_metadata(function=oov_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.int,
-                                name='oov_count')
+                            metadata_type=DatasetMetadataType.int,
+                            name='oov_count')
 
 dataset_binder.set_metadata(function=length_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.int,
-                                name='length')
+                            metadata_type=DatasetMetadataType.int,
+                            name='length')
 
 dataset_binder.set_metadata(function=score_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.int,
-                                name='score')
+                            metadata_type=DatasetMetadataType.int,
+                            name='score')
 
 dataset_binder.set_metadata(function=score_confidence_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.int,
-                                name='score_confidence')
+                            metadata_type=DatasetMetadataType.int,
+                            name='score_confidence')
 
 dataset_binder.set_metadata(function=polarity_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.float,
-                                name='polarity')
+                            metadata_type=DatasetMetadataType.float,
+                            name='polarity')
 
 dataset_binder.set_metadata(function=subjectivity_metadata, subset='IMDBComments',
-                                metadata_type=DatasetMetadataType.float,
-                                name='subjectivity')
+                            metadata_type=DatasetMetadataType.float,
+                            name='subjectivity')
 
 for i in range(len(METRIC_NAMES)):
     dataset_binder.set_metadata(function=metadata_encoder(i), subset='IMDBComments',
                                 metadata_type=DatasetMetadataType.float,
                                 name=METRIC_NAMES[i])
-
