e78e8c9c9c7bd37c4ae1813da06342891de00731
nir-bd <62337638+nir-bd@users.noreply.github.com>
2022-03-20 15:03:07 +0200

    typo

--- examples/coco/coco/pix2pix_upsample.py
+++ examples/coco/coco/pix2pix_upsample.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 
-# This was adapted to the TF platform by changing the TransposeConv2D to upsmaple + conv
+# This was adapted to the Tensorleap platform by changing the TransposeConv2D to upsmaple + conv
 """Pix2pix.
 """
 
