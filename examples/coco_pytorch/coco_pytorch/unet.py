2793054d666ab7bda3931286fa7b00e8b0b7ca27
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-05-23 19:24:53 +0300

    refactor

--- examples/coco_pytorch/coco_pytorch/unet.py
+++ examples/coco_pytorch/coco_pytorch/unet.py
@@ -20,7 +20,6 @@ class conv_block(nn.Module):
         x = self.conv1(inputs)
         x = self.bn1(x)
         x = self.relu(x)
-
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
---
+++ examples/coco_pytorch/coco_pytorch/utils.py
@@ -0,0 +1,27 @@
+import os
+from typing import Tuple
+import torch
+
+SAVE_PATH = '/tensorleap/examples/coco_pytorch/coco_pytorch/models'
+
+# Function to Convert to ONNX
+def save_to_onnx(model: torch.nn.Module, input_size: Tuple[int], model_name: str):
+    # set the model to inference mode
+    model.eval()
+
+    # Let's create a dummy input tensor
+    dummy_input = torch.randn(1, *input_size, requires_grad=True)
+
+    # Export the model
+    torch.onnx.export(model,  # model being run
+                      dummy_input,  # model input (or a tuple for multiple inputs)
+                      os.path.join(SAVE_PATH, f'{model_name}.onnx'),  # where to save the model
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=11,  # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['modelInput'],  # the model's input names
+                      output_names=['modelOutput'],  # the model's output names
+                      dynamic_axes={'modelInput': {0: 'batch_size'},  # variable length axes
+                                    'modelOutput': {0: 'batch_size'}})
+
+    print('Model has been converted to ONNX')
