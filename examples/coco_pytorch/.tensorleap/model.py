d0535c92ad0fd45f79fb4f3b50fd4d385109514a
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-05-23 19:24:53 +0300

    refactor

--- examples/coco_pytorch/.tensorleap/model.py
+++ examples/coco_pytorch/.tensorleap/model.py
@@ -1,14 +1,13 @@
 from pathlib import Path
-from coco_pytorch.model import UnetSegModel
 import tensorflow as tf
+from coco_pytorch.unet import Unet
+from coco_pytorch.utils import save_to_onnx
 
 OUTPUT_CLASSES = 4
 
 def leap_save_model(target_file_path: Path):
     # Load your model
-    # Save it to the path supplied as an argument (has a .h5 suffix)
-    model = UnetSegModel()
-    # model.compile(optimizer='adam',
-    #               loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-    #               metrics=['accuracy'])
+    model = Unet()
+    # Save it to the path supplied as an argument
     model.save(target_file_path)
+
