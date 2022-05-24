9e76f98ba9ac40e603f35f89905a581029e4e4e0
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-05-24 19:53:49 +0300

    onnx save

--- examples/coco_pytorch/.tensorleap/model.py
+++ examples/coco_pytorch/.tensorleap/model.py
@@ -1,13 +1,15 @@
 from pathlib import Path
-import tensorflow as tf
 from coco_pytorch.unet import Unet
 from coco_pytorch.utils import save_to_onnx
 
-OUTPUT_CLASSES = 4
+NUM_CLASSES = 4
+data_shape = (3, 128, 128)
 
 def leap_save_model(target_file_path: Path):
+
     # Load your model
-    model = Unet()
+    model = Unet(NUM_CLASSES)
     # Save it to the path supplied as an argument
-    model.save(target_file_path)
+    save_to_onnx(model, data_shape, 'unet_local', target_file_path)
+
 
