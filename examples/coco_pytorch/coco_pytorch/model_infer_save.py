55cea212c3c1f64b04aea9af6da7eff6ca888cbc
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-05-24 19:57:51 +0300

    add path param to leapcli

---
+++ examples/coco_pytorch/coco_pytorch/model_infer_save.py
@@ -0,0 +1,13 @@
+import torch
+
+from utils import save_to_onnx
+from unet import Unet
+
+def infer_save_model_test():
+    model = Unet(4)
+    input_size = (3, 128, 128)
+    dummy_input = torch.randn(1, *input_size, requires_grad=True)
+    out = model(dummy_input)
+    save_to_onnx(model, (3, 128, 128), 'unet_scratch')
+
+
--- examples/coco_pytorch/coco_pytorch/utils.py
+++ examples/coco_pytorch/coco_pytorch/utils.py
@@ -2,20 +2,21 @@ import os
 from typing import Tuple
 import torch
 
-SAVE_PATH = '/tensorleap/examples/coco_pytorch/coco_pytorch/models'
+
+SAVE_PATH = os.path.join(os.getcwd(), 'models')
 
 # Function to Convert to ONNX
-def save_to_onnx(model: torch.nn.Module, input_size: Tuple[int], model_name: str):
+def save_to_onnx(model: torch.nn.Module, input_size: Tuple[int], model_name: str, path=None):
     # set the model to inference mode
     model.eval()
 
     # Let's create a dummy input tensor
     dummy_input = torch.randn(1, *input_size, requires_grad=True)
-
+    path = os.path.join(SAVE_PATH, f'{model_name}.onnx') if path is None else path  # where to save the model
     # Export the model
     torch.onnx.export(model,  # model being run
                       dummy_input,  # model input (or a tuple for multiple inputs)
-                      os.path.join(SAVE_PATH, f'{model_name}.onnx'),  # where to save the model
+                      path,  # where to save the model
                       export_params=True,  # store the trained parameter weights inside the model file
                       opset_version=11,  # the ONNX version to export the model to
                       do_constant_folding=True,  # whether to execute constant folding for optimization
