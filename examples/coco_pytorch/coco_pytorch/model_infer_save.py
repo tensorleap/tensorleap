246aa652a6e8aa3c272bd19f13bc67c0028b4ff7
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-05-31 15:23:11 +0300

    generelize infer save

--- examples/coco_pytorch/coco_pytorch/model_infer_save.py
+++ examples/coco_pytorch/coco_pytorch/model_infer_save.py
@@ -1,14 +1,12 @@
+from typing import Tuple
+
 import torch
-
 from utils import save_to_onnx
-from unet import Unet
 
-
-def infer_save_model_test():
-    model = Unet(4)
-    input_size = (3, 128, 128)
+# run infer and save locally to test
+def infer_save_model(model: torch.nn.Module, input_size: Tuple[int, int, int], num_classes: int = 4):
     dummy_input = torch.randn(1, *input_size, requires_grad=True)
     out = model(dummy_input)
-    save_to_onnx(model, (3, 128, 128), 'unet_scratch')
+    save_to_onnx(model, input_size, model._get_name())
 
 
