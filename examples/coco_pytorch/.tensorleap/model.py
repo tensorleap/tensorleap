5f279c190980ba612dc0ed32b5f095e0fc0661b4
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-05-31 15:34:10 +0300

    deeplabresnet

--- examples/coco_pytorch/.tensorleap/model.py
+++ examples/coco_pytorch/.tensorleap/model.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from torchvision.models.segmentation import deeplabv3_resnet50
 from coco_pytorch.unet import Unet
 from coco_pytorch.utils import save_to_onnx
 
@@ -8,7 +9,8 @@ data_shape = (3, 128, 128)
 
 def leap_save_model(target_file_path: Path):
     # Load your model
-    model = Unet(NUM_CLASSES)
+    # model = Unet(NUM_CLASSES)
+    model = deeplabv3_resnet50(pretrained=True, pretrained_backbone=True)   # use pytorch pretrained net
     # Save it to the path supplied as an argument
     save_to_onnx(model, data_shape, 'unet_local', target_file_path)
 
