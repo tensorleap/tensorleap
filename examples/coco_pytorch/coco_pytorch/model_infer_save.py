a259b09b46b73251a76a56f3df03bbee6ef3c144
Danielle Ben Bashat <danielle.benbashat@tensorleap.ai>
2022-06-06 13:47:16 +0300

    clean text

--- examples/coco_pytorch/coco_pytorch/model_infer_save.py
+++ examples/coco_pytorch/coco_pytorch/model_infer_save.py
@@ -1,12 +1,23 @@
 from typing import Tuple
-
 import torch
 from utils import save_to_onnx
 
+import imp
+with open('.secret/db.py', 'rb') as fp:
+    db = imp.load_module('.secret', fp, '.tensorleap/dataset.py', \
+    ('.py', 'rb', imp.PY_SOURCE))
+
+
+
 # run infer and save locally to test
-def infer_save_model(model: torch.nn.Module, input_size: Tuple[int, int, int], num_classes: int = 4):
-    dummy_input = torch.randn(1, *input_size, requires_grad=True)
+def _infer_save_model(model: torch.nn.Module, input_size: Tuple[int, int, int], num_classes: int = 4):
+    dummy_input = torch.randn(5, *input_size, requires_grad=True)
     out = model(dummy_input)
     save_to_onnx(model, input_size, model._get_name())
+
+
+
+
+
 
 
--- examples/coco_pytorch/coco_pytorch/utils.py
+++ examples/coco_pytorch/coco_pytorch/utils.py
@@ -1,29 +1,27 @@
 import os
-from typing import Tuple
+from typing import Tuple, Union
 import torch
 
 
-SAVE_PATH = os.path.join(os.getcwd(), 'models')
+SAVE_PATH = os.path.join(os.getcwd())
 
 
 # Function to Convert to ONNX
-def save_to_onnx(model: torch.nn.Module, input_size: Tuple[int], model_name: str, path=None):
+def save_to_onnx(model: torch.nn.Module, input_size: Tuple[int], model_name: Union[str, None], path=None):
     # set the model to inference mode
     model.eval()
-
-    # Let's create a dummy input tensor
     dummy_input = torch.randn(1, *input_size, requires_grad=True)
-    path = os.path.join(SAVE_PATH, f'{model_name}.onnx') if path is None else path  # where to save the model
-    # Export the model
-    torch.onnx.export(model,  # model being run
-                      dummy_input,  # model input (or a tuple for multiple inputs)
-                      path,  # where to save the model
+    model_name = model._get_name() if model_name is None else model_name
+    path = os.path.join(SAVE_PATH, f'{model_name}.onnx') if path is None else path
+    torch.onnx.export(model,
+                      dummy_input,
+                      path,
                       export_params=True,  # store the trained parameter weights inside the model file
                       opset_version=11,  # the ONNX version to export the model to
-                      do_constant_folding=True,  # whether to execute constant folding for optimization
-                      input_names=['modelInput'],  # the model's input names
-                      output_names=['modelOutput'],  # the model's output names
-                      dynamic_axes={'modelInput': {0: 'batch_size'},  # variable length axes
+                      do_constant_folding=True,
+                      input_names=['modelInput'],
+                      output_names=['modelOutput'],
+                      dynamic_axes={'modelInput': {0: 'batch_size'},
                                     'modelOutput': {0: 'batch_size'}})
 
     print('Model has been converted to ONNX')
