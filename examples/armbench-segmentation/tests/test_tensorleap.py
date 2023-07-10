bae9e1ac20ecc9ac22fa783ae10f4c87bc2415fa
ranhomri <ran.homri@tensorleap.ai>
2023-07-10 13:28:03 +0300

    added test with check_dataset added tensorleap yaml file

---
+++ examples/armbench-segmentation/tests/test_tensorleap.py
@@ -0,0 +1,9 @@
+import os
+from code_loader import LeapLoader
+
+if __name__ == '__main__':
+    dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    script_path = 'tensorleap.py'
+    datascript = LeapLoader(code_path=dir_path, code_entry_name=script_path)
+    res = datascript.check_dataset()
+    print(res)
