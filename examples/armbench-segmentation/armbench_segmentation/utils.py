b03ffc290fe9664be368a37e1ead434f7dea1598
ranhomri <ran.homri@tensorleap.ai>
2023-07-10 10:34:30 +0300

    project skeleton

---
+++ examples/armbench-segmentation/armbench_segmentation/utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+from armbench_segmentation import CACHE_DICTS
+from armbench_segmentation.preprocessing import BATCH_SIZE
---
+++ examples/armbench-segmentation/armbench_segmentation/visualizers.py
@@ -0,0 +1,2 @@
+import numpy as np
+from code_loader.contract.visualizer_classes import LeapImageWithBBox
