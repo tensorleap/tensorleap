8c23d1c612782d21112460792ab876e245cd99d9
ranhomri <ran.homri@tensorleap.ai>
2023-07-11 14:36:41 +0300

    edited test as a function and add it to poetry scripts

--- examples/armbench-segmentation/tests/test_tensorleap.py
+++ examples/armbench-segmentation/tests/test_tensorleap.py
@@ -1,9 +1,17 @@
 import os
 from code_loader import LeapLoader
 
-if __name__ == '__main__':
+
+def check_integration():
     dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     script_path = 'tensorleap.py'
     datascript = LeapLoader(code_path=dir_path, code_entry_name=script_path)
     res = datascript.check_dataset()
-    assert res.is_valid
+    if res.is_valid:
+        print("Integration script is valid")
+    else:
+        print(f"Integration failed with error: {res.general_error}")
+
+
+if __name__ == '__main__':
+    check_integration()
