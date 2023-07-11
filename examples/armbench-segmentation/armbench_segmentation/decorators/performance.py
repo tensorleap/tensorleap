573c91553f2c8c8cfffb97c75e3d908d9150d2ed
ranhomri <ran.homri@tensorleap.ai>
2023-07-11 12:18:33 +0300

    timing over multiple runs

--- examples/armbench-segmentation/armbench_segmentation/decorators/performance.py
+++ examples/armbench-segmentation/armbench_segmentation/decorators/performance.py
@@ -3,10 +3,11 @@ import time
 
 def measure_runtime(func):
     def wrapper(*args, **kwargs):
+        n = 10
         start_time = time.time()
-        result = func(*args, **kwargs)
+        result = [func(*args, **kwargs) for _ in range(n)]
         end_time = time.time()
-        runtime = end_time - start_time
+        runtime = round((end_time - start_time) / n, 5)
         print(f"Runtime of {func.__name__}: {runtime} seconds")
         return result
 
