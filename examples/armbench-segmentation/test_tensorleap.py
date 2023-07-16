e211aa237095c3d0ee902ebb743dcd0422910ed1
ranhomri <ran.homri@tensorleap.ai>
2023-07-16 15:50:12 +0300

    refactored path in test file

--- examples/armbench-segmentation/test_tensorleap.py
+++ examples/armbench-segmentation/test_tensorleap.py
@@ -1,9 +1,10 @@
-import os
+from pathlib import Path
+
 from code_loader import LeapLoader
 
 
 def check_integration():
-    dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    dir_path = str(Path(__file__).parent)
     script_path = 'tensorleap.py'
     datascript = LeapLoader(code_path=dir_path, code_entry_name=script_path)
     res = datascript.check_dataset()
