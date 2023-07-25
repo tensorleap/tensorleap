9f7ffbdacd79616555733e0b104cc4acf42a80e4
ranhomri <ran.homri@tensorleap.ai>
2023-07-25 14:10:11 +0300

    refactoring and renaming

--- examples/armbench-segmentation/armbench_segmentation/visualizers/visualizers_getters.py
+++ examples/armbench-segmentation/armbench_segmentation/visualizers/visualizers_getters.py
@@ -1,13 +1,13 @@
 from armbench_segmentation.utils.general_utils import get_mask_list, get_argmax_map_and_separate_masks
 
 
-def mask_visualizer_gt(image, data, mask):
-    bbs, masks = get_mask_list(data, mask, is_gt=True)
+def mask_visualizer_gt(image, bb_gt, masks_gt):
+    bbs, masks = get_mask_list(bb_gt, masks_gt, is_gt=True)
     return get_argmax_map_and_separate_masks(image, bbs, masks)["argmax_map"]
 
 
-def mask_visualizer_prediction(image, data, mask):
-    bbs, masks = get_mask_list(data, mask, is_gt=False)
+def mask_visualizer_prediction(image, y_pred_bbs, y_pred_mask):
+    bbs, masks = get_mask_list(y_pred_bbs, y_pred_mask, is_gt=False)
     return get_argmax_map_and_separate_masks(image, bbs, masks)["argmax_map"]
 
 
