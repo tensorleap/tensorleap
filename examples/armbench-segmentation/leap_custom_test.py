05953239f2ab361480ba7e0fd45fc500bcff7557
ranhomri <ran.homri@tensorleap.ai>
2023-08-09 09:42:23 +0300

    calculate metrics and metadata as dict

--- examples/armbench-segmentation/leap_custom_test.py
+++ examples/armbench-segmentation/leap_custom_test.py
@@ -14,14 +14,8 @@ from armbench_segmentation.visualizers.visualizers import (
 )
 from armbench_segmentation.visualizers.visualizers_getters import mask_visualizer_gt, mask_visualizer_prediction
 from leap_binder import (
-    subset_images, input_image, get_bbs, get_masks, get_cat_instances_seg_lst, get_idx, get_fname, get_original_width,
-    get_original_height, bbox_num, get_avg_bb_area, get_avg_bb_aspect_ratio, get_instances_num,
-    get_object_instances_num, get_tote_instances_num, get_avg_instance_percent, get_tote_instances_masks,
-    get_tote_instances_sizes, get_tote_avg_instance_percent, get_tote_std_instance_percent, get_tote_instances_mean,
-    get_tote_instances_std, get_object_instances_mean, get_object_instances_std, get_object_instances_masks,
-    get_object_instances_sizes, get_object_avg_instance_percent, get_object_std_instance_percent,
-    get_background_percent, get_obj_bbox_occlusions_count, get_obj_bbox_occlusions_avg, get_obj_mask_occlusions_count,
-    count_duplicate_bbs, count_small_bbs
+    subset_images, input_image, get_bbs, get_masks, get_cat_instances_seg_lst, general_metrics_dict,
+    segmentation_metrics_dict, metadata_dict
 )
 
 if __name__ == '__main__':
@@ -48,6 +42,11 @@ if __name__ == '__main__':
     y_pred_masks = y_pred[1]
     y_pred_bb_concat = tf.keras.layers.Permute((2, 1))(y_pred_bbs)  # prepare prediction for further use
 
+    # custom metrics
+    general_metric_results = general_metrics_dict(y_true_bbs, y_pred_bbs, y_true_masks, y_pred_masks)
+    segmentation_metrics_results = segmentation_metrics_dict(input_img_tf, y_pred_bbs, y_pred_masks, y_true_bbs,
+                                                             y_true_masks)
+
     # visualizers
     gt_mask_visualizer_img = mask_visualizer_gt(image, bb_gt, mask_gt)
     predicted_mask_visualizer_img = mask_visualizer_prediction(image, y_pred_bbs[0, ...], y_pred_masks[0, ...])
@@ -56,56 +55,7 @@ if __name__ == '__main__':
     under_segmented_img = under_segmented_bb_visualizer(image, y_pred_bbs[0, ...], y_pred_masks[0, ...], bb_gt, mask_gt)
     over_segmented_img = over_segmented_bb_visualizer(image, y_pred_bbs[0, ...], y_pred_masks[0, ...], bb_gt, mask_gt)
 
-    # custom metrics
-    regression_metric_result = regression_metric(y_true_bbs, y_pred_bbs, y_true_masks, y_pred_masks)
-    classification_metric_result = classification_metric(y_true_bbs, y_pred_bbs, y_true_masks, y_pred_masks)
-    object_metric_result = object_metric(y_true_bbs, y_pred_bbs, y_true_masks, y_pred_masks)
-    mask_metric_results = mask_metric(y_true_bbs, y_pred_bbs, y_true_masks, y_pred_masks)
-    over_segmented_result = over_segmented(input_img_tf, y_pred_bbs, y_pred_masks, y_true_bbs, y_true_masks)
-    under_segmented_result = under_segmented(input_img_tf, y_pred_bbs, y_pred_masks, y_true_bbs, y_true_masks)
-    metric_small_bb_in_under_segment_result = metric_small_bb_in_under_segment(input_img_tf, y_pred_bbs, y_pred_masks,
-                                                                               y_true_bbs, y_true_masks)
-    over_segmented_instances_count_result = over_segmented_instances_count(input_img_tf, y_pred_bbs, y_pred_masks,
-                                                                           y_true_bbs, y_true_masks)
-    under_segmented_instances_count_result = under_segmented_instances_count(input_img_tf, y_pred_bbs, y_pred_masks,
-                                                                             y_true_bbs, y_true_masks)
-    average_segments_num_over_segment_result = average_segments_num_over_segment(input_img_tf, y_pred_bbs, y_pred_masks,
-                                                                                 y_true_bbs, y_true_masks)
-    average_segments_num_under_segmented_result = average_segments_num_under_segmented(input_img_tf, y_pred_bbs,
-                                                                                       y_pred_masks, y_true_bbs,
-                                                                                       y_true_masks)
-    over_segment_avg_confidence_result = over_segment_avg_confidence(input_img_tf, y_pred_bbs, y_pred_masks, y_true_bbs,
-                                                                     y_true_masks)
-
     # metadata functions
     for cat in ['tote', 'object']:
         instances = get_cat_instances_seg_lst(idx, training_response, cat)
-    index = get_idx(idx, training_response)
-    filename = get_fname(idx, training_response)
-    original_width = get_original_width(idx, training_response)
-    original_height = get_original_height(idx, training_response)
-    n_bboxes = bbox_num(idx, training_response)
-    avg_bbox_area = get_avg_bb_area(idx, training_response)
-    avg_bbox_aspect_ratio = get_avg_bb_aspect_ratio(idx, training_response)
-    n_instances = get_instances_num(idx, training_response)
-    n_objects = get_object_instances_num(idx, training_response)
-    n_tote = get_tote_instances_num(idx, training_response)
-    avg_instance_percent = get_avg_instance_percent(idx, training_response)
-    tote_masks = get_tote_instances_masks(idx, training_response)
-    tote_sizes = get_tote_instances_sizes(idx, training_response)
-    tote_avg_instance_percent = get_tote_avg_instance_percent(idx, training_response)
-    tote_std_instance_percent = get_tote_std_instance_percent(idx, training_response)
-    tote_instances_mean = get_tote_instances_mean(idx, training_response)
-    tote_instances_std = get_tote_instances_std(idx, training_response)
-    object_instances_mean = get_object_instances_mean(idx, training_response)
-    object_instances_std = get_object_instances_std(idx, training_response)
-    object_instances_masks = get_object_instances_masks(idx, training_response)
-    object_instances_sizes = get_object_instances_sizes(idx, training_response)
-    object_avg_instance_percent = get_object_avg_instance_percent(idx, training_response)
-    object_std_instance_percent = get_object_std_instance_percent(idx, training_response)
-    background_percent = get_background_percent(idx, training_response)
-    obj_bbox_occlusions_count = get_obj_bbox_occlusions_count(idx, training_response)
-    obj_bbox_occlusions_avg = get_obj_bbox_occlusions_avg(idx, training_response)
-    obj_mask_occlusions_count = get_obj_mask_occlusions_count(idx, training_response)
-    duplicate_bb = count_duplicate_bbs(idx, training_response)
-    small_bbs = count_small_bbs(idx, training_response)
+    metadata = metadata_dict(idx, training_response)
