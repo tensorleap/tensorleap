ebe80d7fcdc4786225b37cd62da3bcd52fd45ab7
Chen Rothschild <chen.rothschild@tensorleap.ai>
2023-08-14 17:06:25 +0300

    delete visuelizers

--- examples/cityscapes_od/leap_binder.py
+++ examples/cityscapes_od/leap_binder.py
@@ -1,16 +1,22 @@
-from typing import List, Dict, Callable, Union
+from typing import Dict, Callable, Union
 from PIL import Image
-import numpy as np
 import json
-import tensorflow as tf
 from tensorflow import Tensor
 from utils_all.gcs_utils import _download
 from utils_all.metrics import regression_metric, classification_metric, object_metric, od_loss, calculate_iou
 from utils_all.preprocessing import Cityscapes, load_cityscapes_data, CATEGORIES, CATEGORIES_no_background, \
     CATEGORIES_id_no_background
-from config import CONFIG
 from utils_all.general_utils import extract_bounding_boxes_from_instance_segmentation_polygons
-from visualizers.visualizers import bb_decoder, gt_bb_decoder, bb_car_gt_decoder, bb_car_decoder
+from typing import List
+import numpy as np
+import tensorflow as tf
+
+from config import CONFIG
+from utils.general_utils import bb_array_to_object, get_predict_bbox_list
+from utils.preprocessing import CATEGORIES_no_background
+
+from code_loader.contract.responsedataclasses import BoundingBox
+from code_loader.contract.visualizer_classes import LeapImageWithBBox
 
 from code_loader import leap_binder
 from code_loader.contract.datasetclasses import PreprocessResponse
@@ -246,6 +252,47 @@ def od_metrics_dict(bb_gt: tf.Tensor, detection_pred: tf.Tensor) -> Dict[str, tf
     for func_name, func in metric_functions.items():
         res[func_name] = func(bb_gt, detection_pred)
     return res
+
+def gt_bb_decoder(image: np.ndarray, bb_gt: tf.Tensor) -> LeapImageWithBBox:
+    """
+    This function overlays ground truth bounding boxes (BBs) on the input image.
+
+    Parameters:
+    image (np.ndarray): The input image for which the ground truth bounding boxes need to be overlaid.
+    bb_gt (np.ndarray): The ground truth bounding box array for the input image.
+
+    Returns:
+    An instance of LeapImageWithBBox containing the input image with ground truth bounding boxes overlaid.
+    """
+    bb_object: List[BoundingBox] = bb_array_to_object(bb_gt, iscornercoded=False, bg_label=CONFIG['BACKGROUND_LABEL'],
+                                                      is_gt=True)
+    bb_object = [bbox for bbox in bb_object if bbox.label in CATEGORIES_no_background]
+    return LeapImageWithBBox(data=(image * 255).astype(np.float32), bounding_boxes=bb_object)
+
+
+def bb_car_gt_decoder(image: np.ndarray, bb_gt: tf.Tensor) -> LeapImageWithBBox:
+    """
+    Overlays the BB predictions on the image
+    """
+    bb_object: List[BoundingBox] = bb_array_to_object(bb_gt, iscornercoded=False, bg_label=CONFIG['BACKGROUND_LABEL'], is_gt=True)
+    bb_object = [bbox for bbox in bb_object if bbox.label == 'car']
+    return LeapImageWithBBox(data=(image * 255).astype(np.float32), bounding_boxes=bb_object)
+
+def bb_decoder(image: np.ndarray, predictions: tf.Tensor) -> LeapImageWithBBox:
+    """
+    Overlays the BB predictions on the image
+    """
+    bb_object = get_predict_bbox_list(predictions)
+    bb_object = [bbox for bbox in bb_object if bbox.label in CATEGORIES_no_background]
+    return LeapImageWithBBox(data=(image * 255).astype(np.float32), bounding_boxes=bb_object)
+
+def bb_car_decoder(image: np.ndarray, predictions: tf.Tensor) -> LeapImageWithBBox:
+    """
+    Overlays the BB predictions on the image
+    """
+    bb_object = get_predict_bbox_list(predictions)
+    bb_object = [bbox for bbox in bb_object if bbox.label == 'car']
+    return LeapImageWithBBox(data=(image * 255).astype(np.float32), bounding_boxes=bb_object)
 
 # ---------------------------------------------------------binding------------------------------------------------------
 
