4c45b6b5ac291e23c4bfc1574ab5a84b4650816a
Chen Rothschild <chen.rothschild@tensorleap.ai>
2023-08-29 14:36:50 +0300

    change to bert model

--- examples/IMDb/IMDb/utils.py
+++ examples/IMDb/IMDb/utils.py
@@ -2,6 +2,10 @@ import numpy as np
 
 import re, string
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from transformers import AutoTokenizer
+
+from IMDb.config import CONFIG
+
 
 def standardize(comment: str) -> str:
     lowercase = comment.lower()
@@ -9,9 +13,10 @@ def standardize(comment: str) -> str:
     punctuation_stripped = re.sub('[%s]' % re.escape(string.punctuation), '', html_stripped)
     return punctuation_stripped
 
-
-def prepare_input(tokanizer, input_text: str, sequence_length: int = 250) -> np.ndarray:
+def prepare_input(tokenizer, input_text: str) -> np.ndarray:
     standard_text = standardize(input_text)
-    tokanized_input = tokanizer.texts_to_sequences([standard_text])
-    padded_input = pad_sequences(tokanized_input, maxlen=sequence_length)
-    return padded_input[0, ...]
+    tokanized_input = tokenizer([standard_text], padding='max_length', truncation=True, max_length=CONFIG['SEQUENCE_LENGTH'])
+    return tokanized_input
+    # tokanized_input = tokenizer.texts_to_sequences([standard_text])
+    # padded_input = pad_sequences(tokanized_input, maxlen=CONFIG['SEQUENCE_LENGTH'])
+    # return padded_input[0, ...]
